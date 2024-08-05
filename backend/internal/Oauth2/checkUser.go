1de00e95584f62e5d012736c4bb859ea83127f45
frnxx1 <mihon2007@list.ru>
2024-08-05 14:07:30 +0500

    fix cookie

--- backend/internal/Oauth2/checkUser.go
+++ backend/internal/Oauth2/checkUser.go
@@ -3,6 +3,7 @@ package oauth2
 import (
 	db "Astral/internal/App/database"
 	md "Astral/internal/jwt/models"
+	ws "Astral/internal/workspace/models"
 	"fmt"
 
 	"github.com/google/uuid"
@@ -16,13 +17,21 @@ type GGUser struct {
 
 func CheckUser(user GGUser) error {
 	var userDB md.User
+	id := uuid.New().String()
 	fmt.Println(userDB)
 	result := db.GlobalDB.Where("email = ?", user.Email).Find(&userDB)
 	if result.RowsAffected == 0 {
 		userDB = md.User{
-			UserId: uuid.New().String(),
+			UserId: id,
 			Email:   user.Email,
 			Name:    user.Name,
+			WorkspaceID: 1,
+			WorkSpace: ws.Workspace{
+				Key: id,
+				Title: "First workspace",
+				Description: "Test",
+				LogoColor: "#ffffff",
+			},
 		}
 		fmt.Println(userDB)
 		res := db.GlobalDB.Create(&userDB)
---
+++ backend/internal/jwt/database/database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	md "Astral/internal/jwt/models"
+	ws "Astral/internal/workspace/models"
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+/* Глобальная переменная для работы с базой данных */
+var GlobalDB *gorm.DB
+
+/* Функция для подключения базы данных */
+func InitDatabase() (err error) {
+	localhost := "localhost"
+	db := os.Getenv("DB_DB")
+	user := os.Getenv("DB_USER")
+	port := os.Getenv("DB_PORT")
+	pass := os.Getenv("DB_PASS")
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s sslmode=disable",
+		localhost,
+		user,
+		db,
+		port,
+		pass,
+	)
+	GlobalDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	err = GlobalDB.AutoMigrate(&md.User{}, &ws.Workspace{}) // Используем структуру из models
+	if err != nil {
+		log.Fatal("Error connecting to the database...", err)
+	}
+	fmt.Println("Database connection successful...")
+	return
+}
